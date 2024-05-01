d711ea2ca289af8ed922138154130e6ed6285bf3
Ryan Lynch <lynch.r.j@gmail.com>
2024-05-01 13:10:05 -0400

    fixed script

--- public/assets/backgrounds/convert.sh
+++ public/assets/backgrounds/convert.sh
@@ -3,9 +3,10 @@ shopt -s extglob
 hue=$1
 saturation=$2
 brightness=$3
+
 for image in !(*.shifted).{jpg,png}; do
   file=$(basename -- "$image")
   without_extension="${file%.*}"
   extension="${file##*.}"
-  convert -define modulate:colorspace=HSB $file -modulate ${saturation:-100},${brighness:-100},$hue $without_extension.$hue.${saturation:-100}.${brighness:-100}.shifted.$extension;
+  convert -define modulate:colorspace=HSB $file -modulate ${brightness:-100},${saturation:-100},$hue $without_extension.$hue.${saturation:-100}.${brightness:-100}.shifted.$extension;
 done;
