57c40c9e30f95095471aff1db642fbcd51937b8a
Ryan Lynch <lynch.r.j@gmail.com>
2024-05-01 14:05:24 -0400

    added blur

--- public/assets/backgrounds/convert.sh
+++ public/assets/backgrounds/convert.sh
@@ -8,5 +8,5 @@ for image in !(*.shifted).{jpg,png}; do
   file=$(basename -- "$image")
   without_extension="${file%.*}"
   extension="${file##*.}"
-  convert -define modulate:colorspace=HSB $file -modulate ${brightness:-100},${saturation:-100},$hue $without_extension.$hue.${saturation:-100}.${brightness:-100}.shifted.$extension;
+  convert -define modulate:colorspace=HSB $file -blur 0x10 -modulate ${brightness:-100},${saturation:-100},$hue $without_extension.$hue.${saturation:-100}.${brightness:-100}.shifted.$extension;
 done;
